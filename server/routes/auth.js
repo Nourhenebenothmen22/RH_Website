7f3a23f1699bd9ea570c20a6466e9cfb43e7e60b
nourhene <benothmennourhen8@gmail.com>
2025-08-13 11:34:29 +0100

    🚀 feat(routes): ajout de la route GET /validate protégée par verifyToken et verifyTokenAdmin

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { userRegister,userLogin } = require('../controllers/authController');
+const { userRegister,userLogin, validateToken } = require('../controllers/authController');
+const { verifyToken, verifyTokenAdmin } = require('../Middlewares/authMiddleware');
 router.post('/register', userRegister);
 router.post('/login', userLogin);
+router.get('/validate',verifyToken,verifyTokenAdmin,validateToken)
 module.exports = router;
