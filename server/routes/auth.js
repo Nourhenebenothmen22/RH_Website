24cb823e59385334ce02ec45276e0b26e0dbc6a7
nourhene <benothmennourhen8@gmail.com>
2025-08-13 13:20:43 +0100

    feat(auth): mise à jour des routes d'authentification (register, login, validate)

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { userRegister,userLogin, validateToken } = require('../controllers/authController');
-const { verifyToken, verifyTokenAdmin } = require('../Middlewares/authMiddleware');
+const { verifyToken } = require('../Middlewares/authMiddleware');
 router.post('/register', userRegister);
 router.post('/login', userLogin);
-router.get('/validate',verifyToken,verifyTokenAdmin,validateToken)
+router.get('/validate',verifyToken,validateToken)
 module.exports = router;
