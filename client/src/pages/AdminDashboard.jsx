f0f277494c40ed64b09a5160e6938b36e1133b51
nourhene <benothmennourhen8@gmail.com>
2025-08-14 14:11:05 +0100

    ✨ Ajout de la page AdminDashboard avec Sidebar, Navbar et Outlet

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useAuth } from '../hooks/useAuth'
+import AdminSidebar from '../components/AdminSidebar'
+import AdminSummary from '../components/AdminSummary'
+import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router-dom'
 
 function AdminDashboard() {
+  const { user } = useAuth()
+  
   return (
-    <div>AdminDashboard</div>
+    <div className="flex min-h-screen">
+      <AdminSidebar />
+      
+      <div className="flex-1 flex flex-col ml-0 md:ml-64">
+        <Navbar />
+        
+        <main className="flex-1 p-6 mt-16">
+          <Outlet/>
+        </main>
+      </div>
+    </div>
   )
 }
 
