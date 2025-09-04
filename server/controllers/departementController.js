8893f6dbb5ec135d21b22b28875adfc3712f8bc1
nourhene <benothmennourhen8@gmail.com>
2025-09-04 12:17:13 +0100

    fix(department): correction de la syntaxe et amélioration des méthodes du contrôleur département

--- server/controllers/departementController.js
+++ server/controllers/departementController.js
@@ -30,6 +30,41 @@ module.exports = {
       console.error("Erreur lors de la récupération des départements:", error);
       res.status(500).json({ error: "Erreur interne du serveur" });
     }
+  },
+  getDepartementById: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const department = await Department.findById(id);
+      if (!department) {
+        return res.status(404).json({ error: "Département non trouvé" });
+      }
+      res.status(200).json({ success: true, department });
+    } catch (error) {
+      console.error("Erreur lors de la récupération du département:", error);
+      res.status(500).json({ error: "Erreur interne du serveur" });
+    }
+  },
+  updatedDepartment:async(req,res)=>{
+    const { id } = req.params;
+    const { dep_name, description } = req.body;
+    try {
+      const updatedDepartment = await Department.findByIdAndUpdate(
+        id,
+        { dep_name, description },
+        { new: true, runValidators: true }
+      );
+      if (!updatedDepartment) {
+        return res.status(404).json({ error: "Département non trouvé" });
+      }
+      res.status(200).json({ success: true, message: "Département mis à jour avec succès", data: updatedDepartment });
+      
+    } catch (error) {
+      console.error("Erreur lors de la mise à jour du département:", error);
+      res.status(500).json({ error: "Erreur interne du serveur" });
+      
+    }
   }
-};
+}
+
+  
 
