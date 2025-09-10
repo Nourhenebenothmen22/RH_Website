126b9762939e3a8a25311c341be03acf1c16b10f
nourhene <benothmennourhen8@gmail.com>
2025-09-10 21:32:03 +0100

    🏢 feat(models): define Department model with Sequelize associations

--- server/models/departement.js
+++ server/models/departement.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const departementSchema = new mongoose.Schema({
     dep_name: { type: String, required: true },
     description: { type: String, required: true },
-    employeeIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Employee" }]
+    employeeIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Employee" }],
+// Référence aux salaires
+  salaries: [{ type: Schema.Types.ObjectId, ref: "Salary" }]
 }, { timestamps: true });
 
 const Department = mongoose.model('Department', departementSchema);
