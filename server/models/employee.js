7d9db9c5364962f92edeb06ee7d185dd7f01298f
nourhene <benothmennourhen8@gmail.com>
2025-09-12 21:45:21 +0100

    feat(employee): add attendances relation to Employee schema

--- server/models/employee.js
+++ server/models/employee.js
@@ -1,6 +1,5 @@
 const { Schema } = require("mongoose");
 const User = require("./user"); // your User model
-
 const employeeSchema = new Schema({
   employeeId: { type: String, required: true, unique: true },
   gender: { type: String },
@@ -9,7 +8,10 @@ const employeeSchema = new Schema({
   department: { type: Schema.Types.ObjectId, ref: "Department"},
   // Reference to salaries
   salaries: [{ type: Schema.Types.ObjectId, ref: "Salary" }],
+  // Reference to projects
   projects: [{ type: Schema.Types.ObjectId, ref: "Project" }],
+  // Reference to attendances
+  attendances: [{ type: Schema.Types.ObjectId, ref: "Attendance" }],
   salary: { type: Number, min: 0 },
   maritalStatus: { type: String }
 });
