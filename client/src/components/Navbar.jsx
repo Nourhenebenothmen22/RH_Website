ff55859141d336300dfbceec2eabe3783a282f0a
nourhene <benothmennourhen8@gmail.com>
2025-09-20 12:49:58 +0100

    🧭 Update Navbar UI/logic

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { FaBell, FaUserCircle, FaSignOutAlt } from 'react-icons/fa'
-import { useAuth } from '../hooks/useAuth'
+import { useAuth } from '../context/AuthContext'
 import { useNavigate } from 'react-router-dom'
 
 function Navbar() {
