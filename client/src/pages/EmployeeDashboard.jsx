62060958c15d86e2bd8dd881bcd228423a9c797d
nourhene <benothmennourhen8@gmail.com>
2025-09-20 12:49:59 +0100

    👨‍💼 Update EmployeeDashboard page

--- client/src/pages/EmployeeDashboard.jsx
+++ client/src/pages/EmployeeDashboard.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import { Outlet } from 'react-router-dom'
-import Sidebar from '../EmployeeDashboard/Sidebar'
 
 function EmployeeDashboard() {
   return (
-     <div className="flex min-h-screen">
-      <Sidebar />
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
       
-      <div className="flex-1 flex flex-col ml-0 md:ml-64">
-        <Navbar />
-        
-        <main className="flex-1 p-6 mt-16">
-          <Outlet/>
-        </main>
-      </div>
+      <main className="flex-1 p-4 md:p-6 mt-16 w-full max-w-full">
+        <Outlet/>
+      </main>
     </div>
   )
 }
