3729acab6f5e76d63b6e9609d4b63153ece47c57
nourhene <benothmennourhen8@gmail.com>
2025-09-27 14:56:24 +0100

    mode environement

--- server/server.js
+++ server/server.js
@@ -19,12 +19,24 @@ const port = process.env.PORT || 5000;
 // ========================
 // 3. Middlewares globaux
 // ========================
+if (process.env.ENV_MODE =="DEVELOPEMENT")
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
+
+else  if (process.env.ENV_MODE ==="PRODUCTION"){
+  app.use(cors({
+  origin: process.env.FRONTEND_URL || 'https://rh-website-qimtbihn7-nourhenes-projects-493ba177.vercel.app/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+}
+
+
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
